29347609a821ea339494636dc9001dd5cc5ac039
nealith <nealith@neaworld.fr>
2018-11-16 22:49:33 +0100

    improvement

--- includes/oRGB.h
+++ includes/oRGB.h
@@ -12,40 +12,61 @@ using namespace cv;
 void convert_oRGB_to_BGR(const Mat & input, Mat & output)
 {
 
+
   output = Mat(input.rows,input.cols,CV_8UC4);
+  Mat output_tmp(input.rows,input.cols,CV_64FC3);
+
+  double max0 = 0.0;
+  double max1 = 0.0;
+  double max2 = 0.0;
+
 
   for (unsigned int i = 0; i < input.rows; i++) {
     const Vec4d * i_ptr_input = input.ptr<Vec4d>(i);
-    Vec4b * i_ptr_output = output.ptr<Vec4b>(i);
+    Vec3d * i_ptr_output_tmp = output_tmp.ptr<Vec3d>(i);
     for (unsigned int j = 0; j < input.cols; j++) {
 
-      double teta = atan2(i_ptr_input[j].val[2],i_ptr_input[j].val[1]);
+      double teta0 = atan2(i_ptr_input[j].val[2],i_ptr_input[j].val[1]);
 
-      double teta0 = 0.0;
+      double teta = 0.0;
 
-      if (teta < PI/2.0){
-        teta0 = (2/3.0) * teta;
+      if (teta0 < PI/2.0){
+        teta = (2.0/3.0) * teta0;
       } else {
-        teta0 = PI/3.0 + (4/3.0)*(teta - PI/2.0);
+        teta = PI/3.0 + (4.0/3.0)*(teta0 - PI/2.0);
       }
 
-      double angle = teta0 - teta;
+      double angle = teta - teta0;
 
       double C1 = cos(angle)*i_ptr_input[j].val[1] - sin(angle)*i_ptr_input[j].val[2];
       double C2 = sin(angle)*i_ptr_input[j].val[1] + cos(angle)*i_ptr_input[j].val[2];
 
-      double R = (double) i_ptr_input[j].val[0] + 0.1140 * C1 + 0.7436 * C2;
-      double G = (double) i_ptr_input[j].val[0] + 0.1140 * C1 - 0.4111 * C2;
-      double B = (double) i_ptr_input[j].val[0] - 0.8660 * C1 + 0.1663 * C2;
+      i_ptr_output_tmp[j].val[2] = i_ptr_input[j].val[0] + 0.1140 * C1 + 0.7436 * C2;
+      if (max2 < i_ptr_output_tmp[j].val[2]) {
+        max2 = i_ptr_output_tmp[j].val[2];
+      }
+      i_ptr_output_tmp[j].val[1] = i_ptr_input[j].val[0] + 0.1140 * C1 - 0.4111 * C2;
+      if (max1 < i_ptr_output_tmp[j].val[1]) {
+        max1 = i_ptr_output_tmp[j].val[1];
+      }
+      i_ptr_output_tmp[j].val[0] = i_ptr_input[j].val[0] - 0.8660 * C1 + 0.1663 * C2;
+      if (max0 < i_ptr_output_tmp[j].val[0]) {
+        max0 = i_ptr_output_tmp[j].val[0];
+      }
 
-      if(B > 255){ B = 255;}
-      if(G > 255){ G = 255;}
-      if(R > 255){ R = 255;}
+    }
+  }
 
-      i_ptr_output[j].val[0] =  (unsigned char) B;
-      i_ptr_output[j].val[1] =  (unsigned char) G;
-      i_ptr_output[j].val[2] =  (unsigned char) R;
-      i_ptr_output[j].val[3] =  0;
+  for (unsigned int i = 0; i < input.rows; i++) {
+    const Vec4d * i_ptr_input = input.ptr<Vec4d>(i);
+    const Vec3d * i_ptr_output_tmp = output_tmp.ptr<Vec3d>(i);
+    Vec4b * i_ptr_output = output.ptr<Vec4b>(i);
+    for (unsigned int j = 0; j < input.cols; j++) {
+
+      i_ptr_output[j].val[0] =  round((i_ptr_output_tmp[j].val[0]/max0)*255);
+      i_ptr_output[j].val[1] =  round((i_ptr_output_tmp[j].val[1]/max1)*255);
+      i_ptr_output[j].val[2] =  round((i_ptr_output_tmp[j].val[2]/max2)*255);
+      i_ptr_output[j].val[3] =  (unsigned char) i_ptr_input[j].val[3];
     }
 
   }
@@ -61,18 +82,18 @@ void convert_BGR_to_oRGB(const Mat & input, Mat & output)
     const Vec4b * i_ptr_input = input.ptr<Vec4b>(i);
     Vec4d * i_ptr_output = output.ptr<Vec4d>(i);
     for (unsigned int j = 0; j < input.cols; j++) {
-      double L = 0.2990 * (double)i_ptr_input[j].val[2] + 0.5870 * (double)i_ptr_input[j].val[1] + 0.1140 * (double)i_ptr_input[j].val[0];
-      double C1 = 0.5000 * (double)i_ptr_input[j].val[2] + 0.5000 * (double)i_ptr_input[j].val[1] - 1.0 * (double)i_ptr_input[j].val[0];
-      double C2 = 0.8660 * (double)i_ptr_input[j].val[2] - 0.8660 * (double)i_ptr_input[j].val[1] + 0 * (double)i_ptr_input[j].val[0];
+      double L = 0.2990 * ((double)i_ptr_input[j].val[2]/255.0) + 0.5870 * ((double)i_ptr_input[j].val[1]/255.0) + 0.1140 * ((double)i_ptr_input[j].val[0]/255.0);
+      double C1 = 0.5000 * (((double)i_ptr_input[j].val[2]/255.0) + ((double)i_ptr_input[j].val[1]/255.0)) - ((double)i_ptr_input[j].val[0]/255.0);
+      double C2 = 0.8660 * (((double)i_ptr_input[j].val[2]/255.0) - ((double)i_ptr_input[j].val[1]/255.0));
 
       double teta = atan2(C2,C1);
 
       double teta0 = 0.0;
 
       if (teta < PI/3.0){
-        teta0 = (3/2.0) * teta;
+        teta0 = (3.0/2.0) * teta;
       } else {
-        teta0 = PI/2.0 + (3/4.0)*(teta - PI/3.0);
+        teta0 = PI/2.0 + (3.0/4.0)*(teta - PI/3.0);
       }
 
       double angle = teta0 - teta;
--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include "oRGB.h"
 
+#include <map>
+#include <list>
+#include <vector>
 
 void createAlphaImage(const cv::Mat& mat, cv::Mat & dst)
 {
@@ -7,75 +10,100 @@ void createAlphaImage(const cv::Mat& mat, cv::Mat & dst)
   cv::split(mat, matChannels);
 
   // create alpha channel
-  cv::Mat alpha = Mat(mat.rows,mat.cols,CV_8UC1,Scalar(0));
+  cv::Mat alpha = Mat(mat.rows,mat.cols,CV_8UC1,Scalar(255));
   matChannels.push_back(alpha);
 
   cv::merge(matChannels, dst);
 }
 
-void search_in_hist(const Mat & hist,int canal,int val, unsigned int & i_min, unsigned int & i_max)
+void match_luminance(const std::vector<std::pair<double,double>> & cdf, double val, unsigned int & i_min, unsigned int & i_max)
 {
-
   if (i_min != i_max && i_min != i_max - 1){
-    unsigned int i_middle = (i_max - i_min) / 2 + i_min;
-    int v_middle = hist.ptr<Vec3i>(i_middle)[0].val[canal];
+    unsigned int i_middle = ((i_max - i_min) / 2.0) + i_min;
+    double v_middle = cdf[i_middle].second;
     if (val == v_middle){
       i_min = i_middle;
       i_max = i_middle;
     } else {
-      int v_min = hist.ptr<Vec3i>(i_min)[0].val[canal];
-      int v_max = hist.ptr<Vec3i>(i_max)[0].val[canal];
-      if (v_min < val && val < v_middle){
+      double v_min = cdf[i_min].second;
+      double v_max = cdf[i_max].second;
+      if (v_min <= val && val < v_middle){
         i_max = i_middle;
       } else {
         i_min = i_middle;
       }
-      search_in_hist(hist,canal,val,i_min,i_max);
+      match_luminance(cdf,val,i_min,i_max);
     }
   }
 }
 
-void compute_stats(const Mat & input ,Mat & hist, Mat & c_hist, Vec3d & mean, Vec3d & sigma){
+bool compare_keys(std::pair<double,double> first,std::pair<double,double> second){
+  return (first.first < second.first);
+}
 
-  hist = Mat(256,1,CV_32SC3,Scalar(0));
-  c_hist = Mat(256,1,CV_32SC3,Scalar(0));
+bool compare_values(std::pair<double,double> first,std::pair<double,double> second){
+  return (first.second < second.second);
+}
 
-  for (unsigned int i = 0; i < input.rows; i++) {
-    const Vec4d * i_ptr_input = input.ptr<Vec4d>(i);
-    for (unsigned int j = 0; j < input.cols; j++) {
-      mean.val[0] += i_ptr_input[j].val[0];
-      mean.val[1] += i_ptr_input[j].val[1];
-      mean.val[2] += i_ptr_input[j].val[2];
+void compute_transfer_data(
+  const Mat & oRGB_input,
+  std::vector<std::pair<double,double>> & cumulative_distribution_function,
+  double & mean_c1,
+  double & mean_c2,
+  double & std_c1,
+  double & std_c2)
+{
+  cumulative_distribution_function = std::vector<std::pair<double,double>>();
+  std::map<double,unsigned int> hist;
+  mean_c1 = 0.0;
+  mean_c2 = 0.0;
+  std_c1 = 0.0;
+  std_c2 = 0.0;
+  for (unsigned int i = 0; i < oRGB_input.rows; i++) {
+    const Vec4d * i_ptr_input = oRGB_input.ptr<Vec4d>(i);
+    for (unsigned int j = 0; j < oRGB_input.cols; j++) {
+      mean_c1 += i_ptr_input[j].val[1];
+      mean_c2 += i_ptr_input[j].val[2];
+      std_c1 += i_ptr_input[j].val[1] * i_ptr_input[j].val[1];
+      std_c2 += i_ptr_input[j].val[2] * i_ptr_input[j].val[2];
 
-      sigma.val[0] += i_ptr_input[j].val[0] * i_ptr_input[j].val[0];
-      sigma.val[1] += i_ptr_input[j].val[1] * i_ptr_input[j].val[1];
-      sigma.val[2] += i_ptr_input[j].val[2] * i_ptr_input[j].val[2];
-
-      hist.ptr<Vec3i>(floor(i_ptr_input[j].val[0]))[0].val[0] += 1;
-      hist.ptr<Vec3i>(floor(i_ptr_input[j].val[1]))[0].val[1] += 1;
-      hist.ptr<Vec3i>(floor(i_ptr_input[j].val[2]))[0].val[2] += 1;
+      if (hist.count(i_ptr_input[j].val[0]) == 0) {
+        hist[i_ptr_input[j].val[0]] = 1;
+      } else {
+        hist[i_ptr_input[j].val[0]] += 1;
+      }
     }
 
   }
 
-  mean.val[0] /= input.rows * input.cols;
-  mean.val[1] /= input.rows * input.cols;
-  mean.val[2] /= input.rows * input.cols;
+  std::list<std::pair<double,double>> distribution_function;
 
-  mean.val[0] /= input.rows * input.cols;
-  mean.val[1] /= input.rows * input.cols;
-  mean.val[2] /= input.rows * input.cols;
+  for (std::map<double,unsigned int>::iterator it=hist.begin(); it != hist.end(); it++) {
+    std::pair<double,double> kv = std::pair<double,double>(it->first,(double)it->second/(double)(oRGB_input.rows * oRGB_input.cols));
+    distribution_function.push_front(kv);
+  }
+  hist.clear();
 
-  mean.val[0] -= mean.val[0] * mean.val[0];
-  mean.val[1] -= mean.val[1] * mean.val[1];
-  mean.val[2] -= mean.val[2] * mean.val[2];
-
-  c_hist.ptr<Vec3i>(0)[0] = hist.ptr<Vec3i>(0)[0];
-  for (unsigned int i = 1; i < 256; i++) {
-    c_hist.ptr<Vec3i>(i)[0] = c_hist.ptr<Vec3i>(i-1)[0] + hist.ptr<Vec3i>(i)[0];
+  distribution_function.sort(compare_keys);
+  std::pair<double,double> last = std::pair<double,double>(-1.0,-1.0);
+  std::pair<double,double> current;
+  for (std::list<std::pair<double,double>>::iterator it=distribution_function.begin(); it != distribution_function.end(); it++) {
+    if (last.first != -1.0) {
+      current = (*it);
+      current.second += last.second;
+    }
+    last = current;
+    cumulative_distribution_function.push_back(last);
   }
 
+  mean_c1 /= oRGB_input.rows * oRGB_input.cols;
+  mean_c2 /= oRGB_input.rows * oRGB_input.cols;
+  std_c1 -= mean_c1 * mean_c1;
+  std_c2 -= mean_c2 * mean_c2;
+  std_c1 = sqrt(std_c1);
+  std_c2 = sqrt(std_c2);
 }
+
 
 
 
@@ -102,55 +130,112 @@ int main( int argc, char** argv )
     convert_BGR_to_oRGB(input,input_oRGB);
     convert_BGR_to_oRGB(style,style_oRGB);
 
-    Mat histInput, histStyle, c_histInput, c_histStyle;
-    Vec3d input_means,style_means,input_sigma, style_sigma;
 
-    compute_stats(input_oRGB,histInput,c_histInput,input_means,input_sigma);
-    compute_stats(style_oRGB,histStyle,c_histStyle,style_means,style_sigma);
+    std::vector<std::pair<double,double>> input_cumulative_distribution_function;
+    double input_mean_c1;
+    double input_mean_c2;
+    double input_std_c1;
+    double input_std_c2;
 
-    output_oRGB = Mat(input.rows,input.cols,CV_64FC4);
-    Mat hist_map(256,1,CV_8UC1,Scalar(0));
+    std::cout << "compute transfer data for input..." << '\n';
+    compute_transfer_data(
+      input_oRGB,
+      input_cumulative_distribution_function,
+      input_mean_c1,
+      input_mean_c2,
+      input_std_c1,
+      input_std_c2
+    );
 
+    std::vector<std::pair<double,double>> style_cumulative_distribution_function;
+    double style_mean_c1;
+    double style_mean_c2;
+    double style_std_c1;
+    double style_std_c2;
 
+    std::cout << "compute transfer data for style..." << '\n';
+    compute_transfer_data(
+      style_oRGB,
+      style_cumulative_distribution_function,
+      style_mean_c1,
+      style_mean_c2,
+      style_std_c1,
+      style_std_c2
+    );
 
-    for (unsigned int i = 0; i < 256; i++) {
-      int val_input = c_histInput.ptr<Vec3i>(i)[0].val[0];
-      unsigned int i_min = 0;
-      unsigned int i_max = 255;
-      search_in_hist(c_histStyle,0,val_input,i_min,i_max);
-      hist_map.ptr<unsigned char>(i)[0] = (unsigned char) i_min;
+    std::cout << "begin histogramm matching..." << '\n';
+
+    std::map<double,double> match;
+
+    for (std::vector<std::pair<double,double>>::iterator it=input_cumulative_distribution_function.begin(); it != input_cumulative_distribution_function.end(); it++) {
+      unsigned int min = 0;
+      unsigned int max = style_cumulative_distribution_function.size();
+
+      match_luminance(style_cumulative_distribution_function,it->second,min,max);
+
+      double match_key = -1.0;
+
+      if (min == max ) {
+        match_key = style_cumulative_distribution_function[max].first;
+      } else {
+        double dmin = it->second - style_cumulative_distribution_function[min].second;
+        double dmax = style_cumulative_distribution_function[max].second - it->second;
+
+        match_key = (style_cumulative_distribution_function[min].first*dmin + style_cumulative_distribution_function[max].first*dmax)/(dmin+dmax);
+      }
+
+      match[it->first] = match_key;
     }
 
-    for (unsigned int i = 0; i < input.rows; i++) {
+    std::cout << "begin output oRGB..." << '\n';
+
+    output_oRGB = Mat(input.rows,input.cols,CV_64FC4);
+
+    double max_1 = 0;
+    double max_2 = 0;
+
+    for (unsigned int i = 0; i < input_oRGB.rows; i++) {
       Vec4d * i_ptr_input = input_oRGB.ptr<Vec4d>(i);
       Vec4d * i_ptr_output = output_oRGB.ptr<Vec4d>(i);
-      for (unsigned int j = 0; j < input.cols; j++) {
-        i_ptr_output[j].val[0] = hist_map.ptr<unsigned char>(floor(i_ptr_input[j].val[0]))[0];
-        i_ptr_output[j].val[1] = (i_ptr_input[j].val[1] - input_means.val[1])*(style_sigma.val[1]/input_sigma.val[1]) + style_means.val[1];
-        i_ptr_output[j].val[2] = (i_ptr_input[j].val[2] - input_means.val[2])*(style_sigma.val[2]/input_sigma.val[2]) + style_means.val[2];
+      for (unsigned int j = 0; j < input_oRGB.cols; j++) {
+        i_ptr_output[j].val[0] = match[i_ptr_input[j].val[0]]; // i_ptr_input[j].val[0];
+        i_ptr_output[j].val[1] = (i_ptr_input[j].val[1] - input_mean_c1)*(style_std_c1/input_std_c1) + style_mean_c1;
+        if (max_1 < i_ptr_output[j].val[1]) {
+          max_1 = i_ptr_output[j].val[1];
+        }
+        i_ptr_output[j].val[2] = (i_ptr_input[j].val[2] - input_mean_c2)*(style_std_c2/input_std_c2) + style_mean_c2;
+        if (max_2 < i_ptr_output[j].val[2]) {
+          max_2 = i_ptr_output[j].val[2];
+        }
         i_ptr_output[j].val[3] = i_ptr_input[j].val[3];
       }
 
     }
 
+    if (max_1 > 1 || max_2 > 1) {
+      std::cout << "ooooooooooo" << '\n';
+    }
 
+    std::cout << "convert to RGBA..." << '\n';
 
     convert_oRGB_to_BGR(output_oRGB,output);
 
-    std::cout << "va te faire enculer" << '\n';
 
-    //imshow( "output", output );
-    //waitKey();
+
+    std::cout << "Writing image in output.png...." << '\n';
 
     bool result = false;
     try
     {
-      result = imwrite("output.png",output);
+        result = imwrite("output.png", output);
     }
     catch (const cv::Exception& ex)
     {
-      fprintf(stderr, "Exception converting image to PNG format: %s\n", ex.what());
+        fprintf(stderr, "Exception converting image to PNG format: %s\n", ex.what());
     }
-
-    return 0;
+    if (result)
+        printf("Saved PNG file with alpha data.\n");
+    else
+        printf("ERROR: Can't save PNG file.\n");
+    return result ? 0 : 1;
 }
